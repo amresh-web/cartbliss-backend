6f6e0fdc888a0def5b1daddfc85b61293bd8d04b
amresh-web <amresh.web@gmail.com>
2024-02-28 07:12:40 +0530

    changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 
 require("dotenv").config();
 
@@ -6,6 +7,7 @@ require("./config/db");
 
 const routes = require("./routes/routes");
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
